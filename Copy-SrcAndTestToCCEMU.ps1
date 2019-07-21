2fa8d4ccedff8b13552b0eb592eeac09ff99043a
Trystan-C <trys.cannon@gmail.com>
2019-07-21 13:17:19 -0700

    Added script to copy code into emulator data folder.

---
+++ Copy-SrcAndTestToCCEMU.ps1
@@ -0,0 +1,26 @@
+$repoPath = "F:\Code\GitHub\CC-ASM";
+$srcDirPath = "$repoPath\src";
+$testDirPath = "$repoPath\test";
+$ccemuDirPath = "C:\Users\Trystan\AppData\Roaming\ccemux\computer\0";
+
+function Delete-DirectoryIfExists($dirPath) {
+    if (Test-Path -Path $dirPath) {
+        Remove-Item $dirPath -Recurse -Force;
+    }
+}
+
+function Copy-DirectoryToPath($fromPath, $toPath) {
+    if (!(Test-Path -Path $fromPath)) {
+        Write-Error "$fromPath does not exist.";
+        return;
+    }
+
+    $fromDirName = Split-Path $fromPath -Leaf;
+    $toDirPath = "$toPath\$fromDirName";
+
+    Delete-DirectoryIfExists $toDirPath;
+    Copy-Item $fromPath $toPath -Recurse;
+}
+
+Copy-DirectoryToPath $srcDirPath $ccemuDirPath;
+Copy-DirectoryToPath $testDirPath $ccemuDirPath;
