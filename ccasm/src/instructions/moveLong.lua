958a5823d03da73fe9e15c7176a753220ce69fb2
Trystan-C <trys.cannon@gmail.com>
2019-10-14 22:03:22 -0700

    Refactored moveLong.

--- ccasm/src/instructions/moveLong.lua
+++ ccasm/src/instructions/moveLong.lua
@@ -4,7 +4,6 @@ apiLoader.loadIfNotPresent("/ccasm/src/utils/integer.lua");
 apiLoader.loadIfNotPresent("/ccasm/src/memory.lua");
 apiLoader.loadIfNotPresent("/ccasm/src/operandTypes.lua");
 apiLoader.loadIfNotPresent("/ccasm/src/registers.lua");
-apiLoader.loadIfNotPresent("/ccasm/src/utils/logger.lua");
 
 byteValue = 2;
 numOperands = 2;
@@ -23,24 +22,34 @@ groupOperandVerifiers = {
     -- TODO
 };
 
-execute = function(from, to)
-    if from.definition == operandTypes.immediateData then
-        if to.definition == operandTypes.dataRegister then
-            local toRegisterId = to.valueBytes[1];
-            tableUtils.zeroPadFrontToSize(from.valueBytes, registers.registerWidthInBytes);
-            registers.dataRegisters[toRegisterId].setLong(from.valueBytes);
-        elseif to.definition == operandTypes.addressRegister then
-            local toRegisterId = to.valueBytes[1];
-            tableUtils.zeroPadFrontToSize(from.valueBytes, registers.registerWidthInBytes);
-            registers.addressRegisters[toRegisterId].setLong(from.valueBytes);
-        end
-    elseif from.definition == operandTypes.symbolicAddress then
-        local offset = integer.getSignedIntegerFromBytes(from.valueBytes);
-        local symbolStartAddress = from.valueStartAddress + offset;
+local function getDataBytesFromOperand(operand)
+    local result;
+    if operand.definition == operandTypes.immediateData then
+        result = tableUtils.zeroPadFrontToSize(operand.valueBytes, 4);
+    elseif operand.definition == operandTypes.symbolicAddress then
+        local offset = integer.getSignedIntegerFromBytes(operand.valueBytes);
+        local symbolStartAddress = operand.valueStartAddress + offset;
         local symbolValue = memory.readBytes(symbolStartAddress, 4);
-        if to.definition == operandTypes.dataRegister then
-            local toRegisterId = to.valueBytes[1];
-            registers.dataRegisters[toRegisterId].setLong(symbolValue);
-        end
+        result = symbolValue;
     end
+    return result;
+end
+
+local function registerId(operand)
+    return operand.valueBytes[1];
+end
+
+local function set(operand)
+    local setter;
+    if operand.definition == operandTypes.dataRegister then
+        setter = registers.dataRegisters[registerId(operand)].setLong;
+    elseif operand.definition == operandTypes.addressRegister then
+        setter = registers.addressRegisters[registerId(operand)].setLong;
+    end
+    return { long = setter };
+end
+
+execute = function(from, to)
+    local dataBytes = getDataBytesFromOperand(from);
+    set(to).long(dataBytes);
 end
