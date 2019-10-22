7a14ed9ed70efc1280b884ca9c8d4abe9643171b
Trystan-C <trys.cannon@gmail.com>
2019-10-21 20:39:04 -0700

    Reduced test duplication.

--- ccasm/test/execute/moveByteTests.lua
+++ ccasm/test/execute/moveByteTests.lua
@@ -4,6 +4,51 @@ local fixture = cpuTestFixture;
 
 local testSuite = {
 
+    moveByteBetweenDataRegisters = function()
+        fixture.assemble([[
+            moveByte #5, d0
+            moveByte d0, d3
+        ]])
+            .load()
+            .step(2)
+            .dataRegister(0).hasValue(5)
+            .dataRegister(3).hasValue(5);
+    end,
+
+    moveByteImmediateToDataRegister = function()
+        fixture.assemble([[
+            moveByte #10, d1
+            moveByte #hFF, d2
+            moveByte #b101, d3
+        ]])
+            .load()
+            .step(3)
+            .dataRegister(1).hasValue(10)
+            .dataRegister(2).hasValue(0xFF)
+            .dataRegister(3).hasValue(5);
+    end,
+
+    moveByteBetweenAddressRegisters = function()
+        fixture.assemble([[
+            moveByte #hA3, a0
+            moveByte a0, a5
+        ]])
+            .load()
+            .step(2)
+            .addressRegister(0).hasValue(0xA3)
+            .addressRegister(5).hasValue(0xA3);
+    end,
+
+    moveByteFromWordToDataRegister = function()
+        fixture.assemble([[
+            moveByte word, d6
+            word declareWord #hABEF
+        ]])
+            .load()
+            .step()
+            .dataRegister(6).hasValue(0xAB);
+    end,
+
     moveByteFromSymbolToDataRegister = function()
         fixture.assemble([[
             moveByte x, d0
--- ccasm/test/execute/moveWordTests.lua
+++ ccasm/test/execute/moveWordTests.lua
@@ -4,6 +4,65 @@ local fixture = cpuTestFixture;
 
 local testSuite = {
 
+    moveWordBetweenDataRegisters = function()
+        fixture.assemble([[
+            moveWord #hABEF, d0
+            moveWord d0, d4
+        ]])
+            .load()
+            .step(2)
+            .dataRegister(0).hasValue(0xABEF)
+            .dataRegister(4).hasValue(0xABEF);
+    end,
+
+    moveWordBetweenAddressRegisters = function()
+        fixture.assemble([[
+            moveWord #hFFFF, a3
+            moveWord a3, a4
+        ]])
+            .load()
+            .step(2)
+            .addressRegister(3).hasValue(0xFFFF)
+            .addressRegister(4).hasValue(0xFFFF);
+    end,
+
+    moveWordFromDataRegisterToAddressRegister = function()
+        fixture.assemble([[
+            moveWord #hABCD, d0
+            moveWord d0, a0
+        ]])
+            .load()
+            .step(2)
+            .dataRegister(0).hasValue(0xABCD)
+            .addressRegister(0).hasValue(0xABCD);
+    end,
+
+    moveWordImmediateToDataRegiser = function()
+        fixture.assemble([[
+            moveWord #25, d0
+            moveWord #h32, d1
+            moveWord #b111, d2
+        ]])
+            .load()
+            .step(3)
+            .dataRegister(0).hasValue(25)
+            .dataRegister(1).hasValue(0x32)
+            .dataRegister(2).hasValue(7);
+    end,
+
+    moveWordImmediateToAddressRegister = function()
+        fixture.assemble([[
+            moveWord #25, a0
+            moveWord #h32, a1
+            moveWord #b111, a2
+        ]])
+            .load()
+            .step(3)
+            .addressRegister(0).hasValue(25)
+            .addressRegister(1).hasValue(0x32)
+            .addressRegister(2).hasValue(7);
+    end,
+
     moveWordFromSymbolToDataRegister = function()
         fixture.assemble([[
             moveWord x, d0
